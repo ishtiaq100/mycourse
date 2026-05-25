5c13e78e35726255958a05ea35f7814bdd6cbf7f
ishtiaq100 <computerbrain200@gmail.com>
2026-05-25 18:23:22 +0500

    stringopt

---
+++ practiceforstringopration.py
@@ -0,0 +1,11 @@
+mobile ="Nokia G200 is the best brand and it compatible for all version it price 45000"
+# print(mobile.upper())
+# print(mobile.lower())
+# print(mobile.capitalize())
+#print(mobile.swapcase)
+print(mobile[6:11])
+print(mobile[76:80])
+print(len(mobile))
+#print(type(mobile))
+# for mob in mobile:
+#     print(mob)
