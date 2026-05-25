022a5a0388768d40947d4fe9616c752bd3eaab6b
ishtiaq100 <computerbrain200@gmail.com>
2026-05-25 17:24:40 +0500

    List practice

---
+++ Listpractice.py
@@ -0,0 +1,10 @@
+mobile = ['samsung',37.5,45000,True]
+mobile.append("china")
+mobile.insert(2,30.9)
+mobile.remove(45000)
+mobile.pop(0)
+for mo in mobile:
+    print(mo)
+print(mobile)
+print(type(mobile))
+print(type(mobile[1]))
