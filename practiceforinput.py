5e6ad8f2b51af63ec1fcb486ce8d3202a232068f
ishtiaq100 <computerbrain200@gmail.com>
2026-06-01 17:05:34 +0500

    update practice

---
+++ practiceforinput.py
@@ -0,0 +1,33 @@
+print("welcome to my practice")
+code = input("please enter your code")
+age = int(input("please ente your age"))
+print(type(age))
+print(age)
+
+if age > 20 :
+    print("gratter")
+elif age < 20:
+    print("younge")
+else: 
+    print("no case")        
+
+newage = age+5;
+print(newage)
+
+newage = age-5;
+print(newage)
+
+newage = age/5;
+print(newage)
+
+newage = age*5;
+print(newage)
+
+print(type(code))
+
+height = float(input("please enter your height"))
+print(type(height))
+print(height)
+print(type(code))
+for cd in code:
+    print(cd)
